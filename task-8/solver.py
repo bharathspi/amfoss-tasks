e6eaf9a10fd3d0d2667ed7de54d649aa3f313a2f
bharathspi <44904973+bharathspi@users.noreply.github.com>
2019-07-31 18:28:45 +0530

    Add files via upload

---
+++ task-8/solver.py
@@ -0,0 +1,55 @@
+
+import pytesseract
+import os
+import argparse
+try:
+    import Image, ImageOps, ImageEnhance, imread
+except ImportError:
+    from PIL import Image, ImageOps, ImageEnhance
+
+def solve_captcha(path):
+
+    """
+    Convert a captcha image into a text, 
+    using PyTesseract Python-wrapper for Tesseract
+
+    Arguments:
+        path (str):
+            path to the image to be processed
+    Return:
+        'textualized' image
+
+    """
+    image = Image.open(path).convert('RGB')
+    image = ImageOps.autocontrast(image)
+
+    filename = "coloured.png".format(os.getpid())
+    image.save(filename)
+
+    text = pytesseract.image_to_string(Image.open(filename))
+    return text
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
+    args = vars(argparser.parse_args())
+    path = args["image"]
+    print('Resolving....')
+    captcha_text = solve_captcha(path)
+    print("below expression is interpreted from image")
+    print(format(captcha_text))
+a=format(captcha_text)
+c=int(a[0])
+d=int(a[2])
+if(a[1]=='+'):
+	result=c+d
+if(a[1]=='-'):
+        result=c-d
+if(a[1]=='*'):
+        result=c*d
+if(a[1]=='/'):
+        result=c/d
+print("result of expression")
+print(result)
+
